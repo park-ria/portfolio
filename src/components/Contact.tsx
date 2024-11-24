b87294c5c95d62f16f9f81e8eaa0970f7087d92f
박리아 <danny6217@gmail.com>
2024-11-25 04:00:46 +0900

    change contact color

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -70,13 +70,12 @@ const IconGroup = styled.ul`
     height: 100px;
     border: 5px solid #fff;
     border-radius: 50%;
-    background: #fff;
     display: flex;
     justify-content: center;
     align-items: center;
+    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5);
     position: relative;
     overflow: hidden;
-    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5);
     cursor: pointer;
     &::before {
       content: "";
@@ -85,30 +84,32 @@ const IconGroup = styled.ul`
       bottom: 0;
       width: 100%;
       height: 0;
+      background: #000;
       transition: all 0.5s;
     }
-    &:nth-child(1)::before {
+    &:nth-child(1) {
       background: #ffe812;
     }
-    &:nth-child(2)::before {
-      background: #000;
+    &:nth-child(2) {
+      background: #662386;
     }
-    &:nth-child(3)::before {
+    &:nth-child(3) {
       background: #20c997;
     }
     &:hover::before {
       height: 100%;
     }
-    &:hover svg {
+
+    svg {
       fill: #fff;
-      transform: rotateY(360deg);
+      transition: all 0.5s;
       &.kakao {
         fill: #000;
       }
     }
-    svg {
-      fill: #000;
-      transition: all 0.5s;
+    &:hover svg {
+      fill: #fff;
+      transform: rotateY(360deg);
     }
   }
 `;
