3510dab5ab4408b174cc44494ebcc049c0c94574
박리아 <danny6217@gmail.com>
2025-04-11 16:52:16 +0900

    url 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,18 +1,19 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  textColor: "#fff",
-  bgColor: "#222",
-  accentColor: "#f1629c",
-  lightGrayColor: "#444",
-  miniIconBg: "#ddd",
-  skillIconBg: "#eee",
-};
 export const lightTheme: DefaultTheme = {
   textColor: "#000",
   bgColor: "#fff",
   accentColor: "#c8f31d",
   lightGrayColor: "#f9f9f9",
+  miniIconBg: "#ddd",
+  skillIconBg: "#eee",
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: "#fff",
+  bgColor: "#222",
+  accentColor: "#f1629c",
+  lightGrayColor: "#444",
   miniIconBg: "#ddd",
   skillIconBg: "#eee",
 };
